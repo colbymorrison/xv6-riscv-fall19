dc9d66f7d5d7acc7609e28b74d87ab728a001830
Colby Morrison <colbyamorrison@gmail.com>
2019-10-14 20:32:31 -0400

    whoops

--- user/nsh.c
+++ user/nsh.c
@@ -1,13 +1,25 @@
 #include "kernel/types.h"
 #include "user/user.h"
 
+#define EXECCMD 1
+
 struct cmd{
   int type;
-  char 
+}
+
+struct execCmd{
+  struct cmd *command;
+  char *name;
+  char **argv;
+}
+
 
 // Execute a command (child)
 void execute(struct cmd*){
-
+  switch(cmd->type){
+    case EXECCMD:
+      exec(cmd->name, cmd->argv) 
+  }
 }
 
 // Parsesa command
@@ -16,18 +28,17 @@ struct cmd* parsecmd(){
 }
 
 int main(int argc, char **argv){
-  int bufSz = 512;
-  char buf[bufSz];
+  char buf[512];
   int fd;
   // Ensure we have stdin, stdout, stderr
-  while((fd = open(xx, O_RDRW)) >= 0){
+  while((fd = open("shell", O_RDRW)) >= 0){
     if(fd >= 3){
       break;
     }
   }
 
   // Execute command
-  while(gets(buf, bufSz)){
+  while(gets(buf, sizeof(buf))){
     // cd ?
     
     // Child executes command
