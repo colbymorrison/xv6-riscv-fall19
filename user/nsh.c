9cb9e05ff329c1562459001a78c0b5fb77937b19
Colby Morrison <colbyamorrison@gmail.com>
2019-10-25 14:26:02 -0400

    improvements pass 3 tests

--- user/nsh.c
+++ user/nsh.c
@@ -11,37 +11,16 @@
 enum { ARG, EOF, EXIT };
 
 // Command structs - inspired by sh.c
-struct cmd{
+struct command{
   int type;
-}
-
-struct execcmd{
-  struct cmd *cmd;
   char *argv[MAXTOKEN];
 };
 
 // Global vars
 char inputBuf[MAXTOKEN*10];
-static char argvbuf[MAXTOKEN*10];
 
 int bufIdx = 0;
 int argvIdx = 0;
-char *argvCur = argvbuf;
-
-//int tokentype;
-int commandType;
-
-// Execute a command (child)
-void executecmd(){
-  switch(commandType){
-    case EXECCMD:
-      exec(argvPt[0], argvPt); 
-      fprintf(2, "nsh: Exec %s failed\n", argvPt[0]);
-      break;
-    default:
-      fprintf(2, "nsh: Exec failed\n");
-  }
-}
 
 // Lexer - inspired by K&R section 5.12
 int isalphanum(char c){
@@ -50,7 +29,7 @@ int isalphanum(char c){
 }
 
 
-void getToken(char *token, int *tokenType){
+int getToken(char *token){
   memset(token, 0, sizeof(token));  
   char cur = inputBuf[bufIdx];
 
@@ -67,10 +46,10 @@ void getToken(char *token, int *tokenType){
       *token++ = cur;
     }
     *token = '\0';
-    *tokenType = ARG;
+    return ARG;
   }
   else if(cur == '\n'){
-    *tokenType = EOF;
+    return EOF;
   }
   // Error case
   else{
@@ -82,42 +61,57 @@ void getToken(char *token, int *tokenType){
 // Recursive-descent parsing
 void parseExec();
 
-void printargv(){
-  for(int i = 0; i < sizeof(argvbuf); i++){
-    printf("argvbuf[%d] = %d ", i, argvbuf[i]);
-    if(i == sizeof(argvbuf)-1){
-      printf("\n");
-    }
-  }
-}
-
-void parseCmd(){
+//void printargv(){
+  //for(int i = 0; i < sizeof(argvbuf); i++){
+    //printf("argvbuf[%d] = %d ", i, argvbuf[i]);
+    //if(i == sizeof(argvbuf)-1){
+      //printf("\n");
+    //}
+  //}
+//}
+//
+void parseCmd(struct command *cmd, char *argvCur){
   char token[MAXTOKEN];
-  int tokentype;
-  getToken(token, &tokentype);
+  int tokentype = getToken(token);
   switch(tokentype){
     case ARG:
-      parseExec(token, tokentype);
+      parseExec(cmd, argvCur, token, tokentype);
       break;
     case EOF:
       break;
   }
 }
 
-void parseExec(char *token, int tokenType){
+void parseExec(struct command *execCmd, char *argvCur, char *token, int tokenType){
   //fprintf(2, "Current token type : %d\n", tokenType);
   //fprintf(2, "Current token value : %s\n", token);
   switch(tokenType){
     case ARG:
       strcpy(argvCur, token);
-      argvPt[argvIdx++] = argvCur;
+      execCmd->argv[argvIdx++] = argvCur;
       argvCur+=strlen(token) + 1;
-      getToken(token, &tokenType);
-      parseExec(token, tokenType);
+      parseExec(execCmd, argvCur, token, getToken(token));
       break;
     case EOF:
-      commandType = EXECCMD;
+      execCmd -> type = EXECCMD;
       break;
+  }
+}
+
+// Execute a command (child)
+void runcmd(){
+  struct command cmd;
+  char argvbuf[MAXTOKEN * 10];
+
+  parseCmd(&cmd, argvbuf);
+
+  switch(cmd.type){
+    case EXECCMD:
+      exec(cmd.argv[0], cmd.argv); 
+      fprintf(2, "nsh: Exec %s failed\n", cmd.argv[0]);
+      break;
+    default:
+      fprintf(2, "nsh: Exec failed\n");
   }
 }
 
@@ -132,18 +126,14 @@ int main(int argc, char **argv){
   }
 
   printf("@ ");
-  // Execute command
   while(gets(inputBuf, sizeof(inputBuf))){
     if(!inputBuf[0]){
         break;
     }
-    // Reset argv Buffer
-    memset(argvbuf, 0, sizeof(argvbuf));
     // cd ?
     // Child executes command
     if(!fork()){
-      parseCmd();
-      executecmd();
+      runcmd();
       exit(0);
     }
     wait(0);
