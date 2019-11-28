7cbed24a127166c947d84e5301a9995efa98ed59
Colby Morrison <colbyamorrison@gmail.com>
2019-11-27 20:53:21 -0500

    schedtest

--- user/schedtest.c
+++ user/schedtest.c
@@ -1,47 +1,46 @@
 #include "kernel/types.h"
 #include "user/user.h"
 
+
 int main(int argc, char **argv){
-  // Fock CPU bound 
-  for(int i = 0; i < 10; i++){
+  // Fork some CPU & IO bound processes 
+  for(int i = 0; i < 3; i++){
     int pid = fork();
-    // Spin for 1000 ticks
     if(pid ==  -1){
       printf("Fork error!\n");
-      exit(1);
+      continue;
     }
 
     if(!pid){
-      printf("uptime\n");
+      // Spin for 10 ticks
       int time = uptime();
-      while(uptime() < time + 1000);
+      while(uptime() < time + 10);
     }
-  }
 
-  //Fork io bound
-  for(int i = 0; i < 20; i++){
-    int pid = fork();
+    pid = fork();
     if(pid == -1){
       printf("Fork error! For rd\n");
-      exit(1);
+      continue;
     }
 
+
     if(!pid){
-      printf("io\n");
-      for(int i = 0; i < 1000000; i++){
+      for(int i = 0; i < 1000; i++){
         int fd;
         if((fd = open("README", 0)) == -1){
-          printf("Open error!\n");
+          printf("Open error\n");
           exit(1);
         }
-        char *buf[30];
-        if(read(fd, buf, sizeof(buf)) == -1){
+        char buf[512];
+        int rc;
+        while((rc = read(fd, buf, sizeof(buf)) > 0));
+        if (rc < 0) {
           printf("Read error!\n");
           exit(1);
         }
+        close(fd);
       }
     }
   }
   exit(0);
 }
-
